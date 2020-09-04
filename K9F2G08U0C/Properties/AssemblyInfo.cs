4207fab08d5a57fc9e190fa95aefdb7314e7edb7
JuliProg <juliprog1110.gmail.com>
2020-09-04 17:31:38 +0000

    automated add new chip

--- K9F2G08U0C/Properties/AssemblyInfo.cs
+++ K9F2G08U0C/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("K9F1G08U0D")]
+[assembly: AssemblyTitle("K9F2G08U0C")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("K9F1G08U0D")]
+[assembly: AssemblyProduct("K9F2G08U0C")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
